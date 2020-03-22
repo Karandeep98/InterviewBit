5dce77c1ff0d0c46e838737580c92faf72db5ef7
Karandeep Singh <karandeep041998@gmail.com>
2020-03-22 19:45:38 +0530

    adding new code

--- 019(Array3Pointers)/main.cpp
+++ 019(Array3Pointers)/main.cpp
@@ -24,30 +24,3 @@ int main()
 
     return 0;
 }
-  /*  vector<vector<int>> a(v.size(),vector<int>(3));
-    int x=0;
-    for(int i=0;i<v.size();i++){
-        for(int j=i+1;j<v.size();j++){
-                for(int k=j+1;k<v.size();k++){
-
-                    if(v[i]+v[j]+v[k]==0){
-                            a[x][0]=v[i];
-                            a[x][1]=v[j];
-                            a[x][2]=v[k];
-                            sort(a[x].begin(),a[x].end());
-                        x++;
-                        break;
-                        }
-                }
-        }
-    }
-    //set<set<int>> s(a.begin(),a.end());
-     //a.assign(a.begin(),a.end());
-    sort(a.begin(),a.end());
-    for(int i=0;i<x;i++){
-        cout<<"[ ";
-            for(int j=0;j<a[i].size();j++)
-                cout<<a[i][j]<<" ";
-    cout<<"]";
-    }*/
-
