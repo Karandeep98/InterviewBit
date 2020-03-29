51ab55d610897d0a00fa0592a774c11f7d56f49d
Karandeep Singh <karandeep041998@gmail.com>
2020-03-30 03:39:03 +0530

    adding new code

--- search/main.cpp
+++ search/main.cpp
@@ -1,12 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
 
-    vector<int> v{1,2,3};
-    set<int>s(v.begin(),v.end());
-    cout<<s[0];
+struct ListNode* head=NULL;
+struct ListNode* a=head;
+
+void print(ListNode* a){
+        while(a!= NULL){
+
+            cout<< a->val ;
+            if(a->next!=NULL) cout<<"-->";
+            a=a->next;
+        }
+        cout<<endl;
+    }
+
+ void insertAtEnd(int d){
+     struct ListNode* temp= new ListNode(0);
+      temp->val=d;
+        temp->next=NULL;
+    if(head==NULL){
+     head=temp;
+     a=head;
+    }
+    else{
+        a->next=temp;
+        a=a->next;
+    }
+ }
+
+
+ int main()
+{
+    ListNode* temp = head;
+    insertAtEnd(1);insertAtEnd(2);insertAtEnd(3);insertAtEnd(4);insertAtEnd(5);insertAtEnd(6);
+    print(head);
     return 0;
 }
 
